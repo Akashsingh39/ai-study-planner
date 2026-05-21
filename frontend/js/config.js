52e07ca8c966bd1fbd3104cf411543aeb6200985
Akash singh <akash@Akashs-MacBook-Air.local>
2026-05-21 05:59:58 +0530

    Update backend URL for Netlify

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 // Change this URL after deploying backend to Render.
-// Local backend: http://localhost:5000
+// Local backend: http://localhost:8000
 // Render backend example: https://your-backend-name.onrender.com
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = "https://ai-study-planner-1-9ixl.onrender.com";
